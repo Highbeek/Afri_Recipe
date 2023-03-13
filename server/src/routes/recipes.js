bc09481ab016405e3d4bdfc2533284d58f7fd290
Ibukun <ibukunagbo@gmail.com>
2023-03-13 16:20:26 +0100

    Africa recipe app

---
+++ server/src/routes/recipes.js
@@ -0,0 +1,27 @@
+import { RecipeModel } from "../models/Recipes.js";
+import mongoose from "mongoose";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await RecipeModel.find({});
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+export { router as recipesRouter };
